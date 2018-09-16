b825c501e6a08635d93239ba0fd7ccaa11f91a8b
Andrew Rothstein <andrew.rothstein@gmail.com>
2018-09-16 17:33:17 -0400

    up to v0.16.0. gussy up dl-checksum. add Windows support. addl CI builds.

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
-VER=0.13.0
+VER=0.16.0
 DIR=~/Downloads
 MIRROR=https://github.com/kelseyhightower/confd/releases/download/v$VER
-wget -O $DIR/confd-$VER-darwin-amd64 $MIRROR/confd-$VER-darwin-amd64
-wget -O $DIR/confd-$VER-linux-amd64 $MIRROR/confd-$VER-linux-amd64
-wget -O $DIR/confd-$VER-windows-amd64 $MIRROR/confd-$VER-windows-amd64
+
+for PLATFORM in darwin-amd64 linux-amd64 windows-amd64.exe
+do
+    wget -O $DIR/confd-$VER-$PLATFORM $MIRROR/confd-$VER-$PLATFORM
+done
 sha256sum $DIR/confd-$VER-*
-#shasum -a 256 $DIR/kubectl-*-$VER
+
